ff32f350ac241b8c243fb6eb1b6daacb60c2b5df
sung-da <desung2020@naver.com>
2022-12-01 20:24:55 +0900

    221201

---
+++ 1. if.js
@@ -0,0 +1,13 @@
+// 조건문 Conditional Statement
+// if(조건) { }
+// if(조건) { } else {}
+// if(조건) { } else if(조건) {} else {}
+
+let fruit = 'orange';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else if (fruit === 'orange') {
+  console.log('🍊');
+} else {
+  console.log('😍');
+}
